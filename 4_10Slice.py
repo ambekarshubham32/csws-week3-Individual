58fb6d2850ab9bcc012e10075cb9d241ac3df864
ambekarshubham30@gmail.com <ambekarshubham30@gmail.com>
2022-02-03 16:00:19 +0000

    Slice done

---
+++ 4_10Slice.py
@@ -0,0 +1,8 @@
+m=["The", "first","three", "items", "in", "the", "list", "are"]
+print(m[:3])
+
+m1=["Three", "items" ,"from", "the", "middle", "of", "the" ,"list"]
+print(m1[1:5])
+
+m2=["The","last","three", "items", "in","the","list","are"]
+print(m2[-3:])
